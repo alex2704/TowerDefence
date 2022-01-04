8b4820d9b88ee54ada4f54a3120105cc4948d8d9
Sasha <34662549+alex2704@users.noreply.github.com>
2022-01-05 01:23:12 +0300

    Added shop;

---
+++ Assets/Shop.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Standard selected");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    }
+
+    public void PurchaseAnotherTurret()
+    {
+        Debug.Log("selected another");
+        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+    }
+}
